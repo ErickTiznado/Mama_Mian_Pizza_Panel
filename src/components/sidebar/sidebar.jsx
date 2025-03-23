9c2c35f9066ff16a2ab54a10dbcee27c2a46a74f
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2025-03-23 09:38:14 -0600

    Refactorizar el componente de barra lateral para usar botones para la navegación y actualizar las variables del esquema de colores para mantener consistencia

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -4,46 +4,34 @@ import { House, ShoppingCart, Users, ClipboardList, Package, ChartLine, Store  }
 function Sidebar() {
     return(
         <div className='sidebar'>
-            <div className="items">
+            <button className="items">
                 <House size={38} />
-                <span>
-                    Inicio
-                </span>
-            </div>
-            <div className="items">
+                Inicio
+            </button>
+            <button className="items">
             <ShoppingCart  size={38} />
-            <span>Pedidos</span>
-            </div>
-            <div className="items">
+            Pedidos
+            </button>
+            <button className="items">
                 <ClipboardList size={38}  />
-                <span>
                     Contenido
-                </span>
-            </div>
-            <div className="items">
+            </button>
+            <button className="items">
                 <Package size={38}  />
-                <span>
                     Inventario
-                </span>
-            </div>
-            <div className="items">
+            </button>
+            <button className="items">
                 <ChartLine size={38}  />
-                <span>
                     Informes y Estadisticas
-                </span>
-            </div>
-            <div className="items">
+            </button>
+            <button className="items">
                 <Users  size={38} />
-                <span>
                     Clientes
-                </span>
-            </div>
-            <div className="items">
+            </button>
+            <button className="items">
                 <Store size={38}  />
-                <span>
                     Visualizar Tienda
-                </span>
-            </div>
+            </button>
         </div>
     );
 }
